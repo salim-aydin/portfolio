37916fdf6f4e498a426bd05023aea4a4f6c946aa
salim-aydin <salimaydin340@gmail.com>
2023-05-07 03:42:23 +0300

    fixed 2

--- app/projects/reactt/page.jsx
+++ app/projects/reactt/page.jsx
@@ -5,8 +5,8 @@ const Reactt = () => {
   return (
     <div className='container mx-auto max-w-screen-md px-4 py-10'>
       <div className='grid grid-cols-3 gap-6'>
-        <Link href="/projects/reactt/box1" className='w-full h-20 md:h-64 p-2 md:p-4 border border-gray-300 hover:border-gray-400 hover:bg-gray-100 cursor-pointer flex justify-center items-center'>
-          Box 1
+        <Link href="/projects/reactt/box1" className='w-full h-20 md:h-64 p-2 md:p-4 border border-gray-300 hover:border-gray-400 hover:bg-gray-100 cursor-pointer flex justify-center items-center text-center'>
+          Random Number Generator
         </Link>
         <Link href="/projects/reactt/box2" className='w-full h-20 md:h-64 p-2 md:p-4 border border-gray-300 hover:border-gray-400 hover:bg-gray-100 cursor-pointer flex justify-center items-center'>
           Box 2
